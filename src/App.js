9b539de3f3a62dca4b23af2eb1ea3fc1af66ce67
RoiYehezkel <roi5634@gmail.com>
2023-08-15 19:56:08 +0300

    create components files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 function App() {
-  return <div className="App">react</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
