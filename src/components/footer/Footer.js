1f6ddcb6b13137cdcc7a77d43eeb84a1bdeeaa6a
RoiYehezkel <roi5634@gmail.com>
2023-08-16 20:02:14 +0300

    footer section added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,66 @@
 import React from "react";
 import "./Footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#home" className="footer_logo">
+        ROI YEHEZKEL
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonial">Testimonial</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a
+          href="https://www.linkedin.com/in/roi-yehezkel-b97056231/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/RoiYehezkel"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100001367017876"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebookSquare />
+        </a>
+      </div>
+
+      <div className="footer_copyright">© Roi Yehezkel</div>
+    </footer>
+  );
 }
 
 export default Footer;
